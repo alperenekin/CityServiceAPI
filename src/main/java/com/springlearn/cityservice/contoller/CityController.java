e076b4bfbcd80dbdbee6398eae57efdc6a3438bb
Alperen Ekin <alperenekin97@gmail.com>
2021-04-02 01:52:07 +0300

    Can post a city

--- src/main/java/com/springlearn/cityservice/contoller/CityController.java
+++ src/main/java/com/springlearn/cityservice/contoller/CityController.java
@@ -1,4 +1,50 @@
 package com.springlearn.cityservice.contoller;
 
+import com.springlearn.cityservice.model.City;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cities")
 public class CityController {
+
+
+    private final List<City> cities = new ArrayList<>();
+
+    public CityController(){
+        if(cities.isEmpty()){
+            City city1 = new City(new Date(),"06","Ankara");
+            City city2 = new City(new Date(), "34","Istanbul");
+            cities.add(city1);
+            cities.add(city2);
+        }
+
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<City>> getCities(){
+        return  new ResponseEntity<>(cities, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<City> getCity(@PathVariable String id){ //both has "id" keyword.
+        City resultCity = cities.stream()  //for each le de yapılabilirdi.
+                .filter(city -> city.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("City could not be found"));
+        return  new ResponseEntity<City>(resultCity, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<City> createCity(@RequestBody City newCity){
+        newCity.setCreateDate(new Date());
+        cities.add(newCity);
+        return new ResponseEntity<>(newCity,HttpStatus.CREATED);
+    }
 }
